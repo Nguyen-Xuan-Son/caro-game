56c0034353f5de3e11c4913196453399e6b983fe
son.nguyen.xuan <son.nguyen.xuan@ntq-solution.com.vn>
2020-02-17 16:36:59 +0700

    .

--- src/server.js
+++ src/server.js
@@ -2,15 +2,11 @@ const express = require('express');
 const http = require('http');
 const app = express();
 
-const initSocketIO = require('./socket');
-const initController = require('./controller');
-const initMiddleware = require('./config/middleware');
-
 const server = http.createServer(app);
 
-initMiddleware(app);
-initSocketIO(server);
-initController(app);
+require('./config/middleware')(app);
+require('./socket')(server);
+require('./controller')(app);
 
 server.listen(process.env.PORT || 9000, () => {
     console.log(`Server started on port ${server.address().port} :)`);
