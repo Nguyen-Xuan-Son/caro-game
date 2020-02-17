7c5ec2c1dd8e6f1d5284ed376f11e02669e8a80b
son.nguyen.xuan <son.nguyen.xuan@ntq-solution.com.vn>
2020-02-17 15:26:29 +0700

    .

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const http = require('http');
 // const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // app.use(cors());
 
 const server = http.createServer(app);
 
+app.use(bodyParser.json());
 const io = require('socket.io').listen(server);
 
 io.sockets.on('connection', (ws) => {
@@ -17,7 +19,15 @@ io.sockets.on('connection', (ws) => {
             ws.emit('timer', new Date().getSeconds());
         }, 1000);
     });
+});
 
+app.post('/', (req, res) => {
+    console.log('req.body', req.body);
+    // console.log('res', res);
+    res.json({
+        ok: true,
+        games: 'games'
+    });
 });
 
 server.listen(process.env.PORT || 9000, () => {
